998e01c81b5b280a3dc812fc3887b550a8f557fe
thosakwe <thosakwe@gmail.com>
2017-07-19 17:39:40 -0400

    Still working on it.

---
+++ example/todo.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:angel_framework/angel_framework.dart';
+import 'package:angel_hot/angel_hot.dart';
+import 'package:angel_graphql/angel_graphql.dart';
+
+main() async {
+  var hot = new HotReloader(() async {
+    var app = new Angel()..storeOriginalBuffer = true;
+    app.use('/api/todos', new MapService());
+    app.all('/graphql', graphQLHTTP('api/todos', graphiql: false));
+    app.after.add(() => throw new AngelHttpException.notFound());
+
+    app.errorHandler =
+        (AngelHttpException e, RequestContext req, ResponseContext res) async {
+      res
+        ..statusCode = e.statusCode
+        ..write(e.message)
+        ..end();
+    };
+
+    app.fatalErrorStream.listen((e) {
+      stderr..writeln('FATAL: ${e.error}')..writeln(e.stack);
+      new Future.sync(() {
+        e.request.response..writeln('FATAL: ${e.error}')..writeln(e.stack);
+        return e.request.response.close();
+      }).catchError((res) {
+        // Whoops!
+        stderr.writeln('Couldn\'t set fatal message: $res');
+      });
+    });
+
+    return app;
+  }, [Directory.current]);
+
+  var server = await hot.startServer(InternetAddress.LOOPBACK_IP_V4, 3000);
+  print(
+      'GraphQL example listening at http://${server.address.address}:${server.port}');
+}
--- lib/angel_graphql.dart
+++ lib/angel_graphql.dart
@@ -3,6 +3,7 @@ import 'package:angel_framework/angel_framework.dart';
 import 'package:angel_validate/angel_validate.dart';
 import 'package:graphql_parser/graphql_parser.dart';
 import 'package:graphql_schema/graphql_schema.dart';
+import 'src/visitor.dart';
 
 final ContentType graphQlContentType =
     new ContentType('application', 'graphql');
@@ -13,15 +14,72 @@ final Validator graphQlPostBody = new Validator({
   'variables': isMap
 });
 
-/// Mounts a GraphQL API.
-RequestHandler graphQLHTTP(GraphQLSchema schema, {bool graphiql: true}) {
+/// Mounts a GraphQL API that queries the [Service] at the given [servicePath].
+///
+/// If [graphiql] is set to `true`, then responses will fallback to rendering the GraphIQL interface.
+/// If it is `null`, then it will default to `!app.isProduction`
+RequestHandler graphQLHTTP(Pattern servicePath,
+    {GraphQLSchema schema, bool graphiql}) {
+  Service _service;
+  bool _showGraphiql = graphiql;
+
   return (RequestContext req, ResponseContext res) async {
     if (req.method != 'GET' && req.method != 'POST')
       throw new AngelHttpException.methodNotAllowed(
           message: 'This GraphQL endpoint only accepts GET and POST requests.');
 
     DocumentContext queryDoc;
+    _service ??= req.app.service(servicePath);
+    _showGraphiql ??= !req.app.isProduction;
 
+    if (req.method == 'GET') {
+      // Use `uri.queryParameters` because `body_parser` can't handle this.
+      if (req.uri.queryParameters.containsKey('query')) {
+        queryDoc =
+            new Parser(scan(req.uri.queryParameters['query'])).parseDocument();
+      } else if (_showGraphiql) {
+        // TODO: GraphiQL
+        return 'graphiql';
+      } else {
+        throw new AngelHttpException.badRequest(
+            message: 'Expected "query" in the request query string.');
+      }
+    }
+
+    // Different scenario for POST, because we can write data.
+    else {
+      if (req.headers.contentType?.mimeType == graphQlContentType.mimeType) {
+        var buf = await req.lazyOriginalBuffer();
+
+        if (buf == null) {
+          throw new StateError(
+              'graphQLHTTP cannot parse application/graphql bodies if app.storeOriginalBuffer is not `true`.');
+        }
+
+        var queryString = new String.fromCharCodes(buf);
+        print('Q: $queryString');
+        queryDoc = new Parser(scan(queryString)).parseDocument();
+      } else {
+        try {
+          var data = graphQlPostBody.enforce(await req.lazyBody());
+          queryDoc = new Parser(scan(data['query'])).parseDocument();
+        } on ValidationException catch (e) {
+          throw new AngelHttpException.badRequest(
+              message: e.message, errors: e.errors);
+        }
+      }
+    }
+
+    // Execute...
+    if (queryDoc == null) {
+      // TODO: Throw error
+    } else {
+      // TODO: Which data to query???
+      var data = {};
+      return const QueryVisitor().visitDocument(queryDoc, data);
+    }
+
+    return false;
     if (req.query.containsKey('query')) {
       queryDoc = new Parser(scan(req.query['query'])).parseDocument();
     } else if (req.method == 'GET') {
